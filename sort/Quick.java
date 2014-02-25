fd04b8a9b096672104bd5046c203e6cc5da6809a
kasaquan <kasajq@gmail.com>
2014-02-25 17:50:28 +0100

    selection function added

--- sort/Quick.java
+++ sort/Quick.java
@@ -39,4 +39,18 @@ public class Quick{
             return true;
         else return false;
     }
+    // selection the K th largest in linear time based on partition
+    public static Comparable select(Comparable[] a, int k)
+    {
+        StdRandom.shuffle(a);
+        int lo = 0, hi = a.length-1;
+        while (hi > lo)
+        {
+            int j = partition(a, lo, hi);
+            if (j > k ) hi = j-1;
+            else if (j < k) lo = j+1;
+            else return a[k];
+        }
+        return a[k];
+    }
 }

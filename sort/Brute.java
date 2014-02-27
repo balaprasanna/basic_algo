eb97c1b1f519033f1c55a4ec4b5060ee576a7935
kasaquan <kasajq@gmail.com>
2014-02-27 01:12:37 +0100

    try to reduce redundency in fast

--- sort/Brute.java
+++ sort/Brute.java
@@ -8,35 +8,30 @@ public class Brute {
        Point[] points = new Point[N];
        int x, y;
        int ctr = 0;
+       StdDraw.setXscale(0, 32768); // rescale the coordinate system
+       StdDraw.setYscale(0, 32768);
        while(!in.isEmpty()){
            x = in.readInt();
            y = in.readInt();
-           points[ctr++] = new Point(x, y);
+           points[ctr] = new Point(x, y);
+           points[ctr].draw();
+           ctr++;
        }
        
-       StdDraw.setXscale(0, 32768); // rescale the coordinate system
-       StdDraw.setYscale(0, 32768);
+       
        for(int i = 0; i < N-3; i++)
        {    
-            //StdOut.print("i=");
-            //StdOut.println(i);
             Point p = points[i];
             for(int j = i+1; j < N-2; j++)
             {
-                //StdOut.print("j=");
-                //StdOut.println(j);
                 double s1 = p.slopeTo(points[j]);
                 for(int m = j+1; m < N-1; m++)
                 {
-                    //StdOut.print("m=");
-                    //StdOut.println(m);
                     double s2 = p.slopeTo(points[m]);
                     if(Double.compare(s1, s2) == 0)
                     {
                         for (int n = m+1; n < N; n++)
                         {
-                            //StdOut.print("n=");
-                            //StdOut.println(n);
                             if(Double.compare(s2, p.slopeTo(points[n])) == 0)
                             {
                                 Point[] good = new Point[4];
@@ -46,13 +41,9 @@ public class Brute {
                                 good[3] = points[n];
                                 Arrays.sort(good);
                                 for(int k=0; k < 3; k++)
-                                {
-                                    good[k].draw();
-                                    good[k].drawTo(good[k+1]);
                                     StdOut.print(good[k].toString() + " -> ");
-                                }
-                                good[3].draw();
                                 StdOut.println(good[3].toString());
+                                good[0].drawTo(good[3]);
                             } 
                         }
                     }
--- sort/Fast.java
+++ sort/Fast.java
@@ -1,21 +1,58 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Fast {
+    private static ArrayList<Dummy> d = new ArrayList<Dummy>();
+    private class  Dummy{
+        public String str; //the result of p.toString, p is the largest point
+        public Double ang; // the angle when print as segment  
+        public Dummy(String s, Double a){
+            str = s;
+            ang = a;
+        }
+       
+    }
+     //my own version of contains
+    private boolean checkdum(Dummy y){
+        if (y == null) return false;
+        if (d.size() == 0) return false;
+        for (int i = 0; i < d.size(); i++)
+        {
+            Dummy x = d.get(i);
+            if ((x.str.compareTo(y.str) == 0) &&
+                 (Double.compare(x.ang, y.ang) == 0))
+                return true;
+         }
+         return false;
+    }
+    
     public static void main(String[] args)
     {
+        
+        Fast name = new Fast();
+        Fast.Dummy now = null;
         String filename = args[0];
         In in = new In(filename);
         int N = in.readInt();
         Point[] points = new Point[N];
         int x, y;
         int num = 0;
+        StdDraw.setXscale(0, 32768); // rescale the coordinate system
+        StdDraw.setYscale(0, 32768);
         while(!in.isEmpty()){
            x = in.readInt();
            y = in.readInt();
-           points[num++] = new Point(x, y);
+           points[num] = new Point(x, y);
+           points[num].draw(); //each point has to be drawn excatly once.
+           num++;
         }
-        StdDraw.setXscale(0, 32768); // rescale the coordinate system
-        StdDraw.setYscale(0, 32768);
+        //sort points according to natrual order
+        Arrays.sort(points);
+
+        
+        //d is the list for redundency checking
+        ArrayList<Dummy> d = new ArrayList<Dummy>();
+        
         //the three very last points cannot start a qualifying segment
         for (int i = 0; i < N-3; i++)  
         {
@@ -37,27 +74,32 @@ public class Fast {
                 } 
                 if (ctr >= 3)
                 {
-                    //redundency?
-                    
+                    //remove redundency
+                    //StdOut.println(angs[j-1]);
                     //copy the qualified ones to good
                     Point[] good = new Point[ctr+1];
                     good[0] = current;
                     for(int l = 1; l <ctr+1; l++)
                         good[l] = copy[j-ctr+l];
                     Arrays.sort(good);
+                    //create relevant dummy
                     
-                    //print and draw the starting point
-                    for(int l = 0; l< good.length-1; l++)
+                    now = name.new Dummy(good[good.length-1].toString(), angs[j-1]);
+                    //StdOut.println(good[good.length-1].toString());
+                    //StdOut.println(angs[j-1]);
+                    if (!name.checkdum(now))
                     {
-                        good[l].draw();
-                        good[l].drawTo(good[l+1]);
-                        StdOut.print(good[l].toString() + " -> "); 
+                        for(int l = 0; l< good.length-1; l++)
+                            StdOut.print(good[l].toString() + " -> "); 
+                        good[0].drawTo(good[good.length-1]);
+                        StdOut.println(good[good.length-1].toString()); 
+                        name.d.add(now);
+                        //StdOut.println(name.d.size());
                     }
-                    good[good.length-1].draw();
-                    StdOut.println(good[good.length-1].toString()); 
                 }
                 ctr = 1; //counter reset
             }
         }
     }
+
 }

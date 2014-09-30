37f59d378f54e3cde08ca2e1bc10294864d309cf
kasaquan <kasajq@gmail.com>
2014-09-30 23:14:30 +0200

    109

---
+++ leetcode/BSTLevelOrderTrav.java
@@ -0,0 +1,29 @@
+
+public class BSTLevelOrderTrav {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Queue<TreeNode> qt = new LinkedList<TreeNode>();
+        int ctr1 = 0, ctr2=1;
+        if(root == null) return result;
+        qt.add(root);
+        while(qt.size() > 0){
+            ctr1 = ctr2; ctr2 = 0;
+            TreeNode cur = null;
+            List<Integer> level = new ArrayList<Integer>();
+            for(;ctr1 > 0; ctr1 --){
+                cur = qt.poll();
+                level.add(cur.val);
+                if(cur.left!= null){
+                    qt.add(cur.left);
+                    ctr2++;
+                }
+                if(cur.right!=null){
+                    qt.add(cur.right);
+                    ctr2++;
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}

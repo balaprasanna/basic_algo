a4ba8c98d968d0827b8b387561eb838524112e66
kasaquan <kasajq@gmail.com>
2014-10-05 12:00:03 +0200

    144 R bit hacker

---
+++ leetcode/SingleNumber.java
@@ -0,0 +1,11 @@
+//need further exploring about bit hacker
+public class SingleNumber {
+	public int singleNumber(int[] A) {
+        int ones=0, twos =0;
+        for(int i =0; i < A.length; i++){
+            ones = (ones ^ A[i]) & ~twos;
+            twos = (twos ^A[i]) & ~ones;
+        }
+        return ones;
+    }
+}
